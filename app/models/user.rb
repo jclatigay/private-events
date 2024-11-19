dd0a803559082014e6fba61cbd93779727fb81af
John Isaac Latigay <jclatigay@up.edu.ph>
2024-11-19 17:47:07 +0800

    created models

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :created_events, class_name: "Event", foreign_key: "creator_id"
+  has_many :event_attendances
+  has_many :attended_events, through: :event_attendances, source: :event
+end
